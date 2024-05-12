496d06d75e85074ed97ca50f3637ffd01375acff
Xayrullo Jaloldinov <xayrullo.jaloldinov@gmail.com>
2024-05-12 23:16:56 +0500

    Added SASS

--- app.vue
+++ app.vue
@@ -1,3 +1,9 @@
 <template>
   <div class="text-red-400">Lorem ipsum dolor sit.</div>
+  <div class="app-text">Lorem ipsum dolor sit amet.</div>
 </template>
+<style lang="scss" scoped>
+.app-text {
+  color: green;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ["~/assets/style/tailwind.css"],
+  css: ["~/assets/style/tailwind.css", "~/assets/style/main.scss"],
   vite: {
     vue: {
       customElement: true,
